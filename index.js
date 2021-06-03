094e5f478edfc4700e401da86e9f925522dec0a4
Sai Ashwin <sai.ashwin2409@gmail.com>
2021-06-03 10:13:10 +0530

    removed comments with old passwords

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var PlaceSearch = require("./GooglePlaceSearch.js"); // This is the file which makes the HTTP request to Google to search for schools
-var config = require("./config.js"); // This file contains the variables required.
-var request = require("request"); // Makes HTTP requests
-var req =  require("sync-request"); // Makes Synchronous http requests , my sister wanted this, idk why
-var placeSearch = new PlaceSearch(config.apiKey, config.outputFormat); // Creating object of GooglePlaceSearch 
-var write = require("./Write.js"); // Linking file which writes data to file
-var fs = require("fs"); // Filesystem library
-var writeToFile = new write(fs); // Making a filesystem object
+var PlaceSearch = require("./GooglePlaceSearch.js"); 
+var config = require("./config.js"); 
+var request = require("request"); 
+var req =  require("sync-request");
+var placeSearch = new PlaceSearch(config.apiKey, config.outputFormat); /
+var write = require("./Write.js"); 
+var fs = require("fs"); 
+var writeToFile = new write(fs); 
 
 var distances = [];
 var i,j;
@@ -13,15 +13,15 @@ parameters = {
         location: [13.0823, 80.2754],
         types: "school",
         radius: 50000 
-    }; // These are the paramenters. Location gives Google the coordinates of Chennai Central, radius will define radius in which to find schools
+    };
 placeSearch(parameters, function (error, response) {
         var length = Object.keys(response.results).length;
         if(error) throw error;
         for(i = 0; i < 10; i++){
-           var dest = 'place_id:' + response.results[i].place_id; // Making a string containing the destination (School)
+           var dest = 'place_id:' + response.results[i].place_id; 
             for(j = i+1; j < 10; j++){
-                var origin = 'place_id:' + response.results[j].place_id; // String containing the Origin
-                var url = config.distanceAPI +  dest + "&destinations=" + origin + "&key=" + config.apiKey; // Making the API request URL
+                var origin = 'place_id:' + response.results[j].place_id; 
+                var url = config.distanceAPI +  dest + "&destinations=" + origin + "&key=" + config.apiKey; 
                 /*request(url, function (err, result, body) {
                     if(err) throw err; // Throw ERROR if any
                     if (!error && result.statusCode == 200) {
@@ -29,8 +29,8 @@ placeSearch(parameters, function (error, response) {
                     distances.push(distancesElement); //This pushes the "rows" object of each school returned
                     }
                 });*/
-                var res = req("GET", url); // sister why
-                console.log(res.getBody('utf8')); // Here you go you forgot encryption you dumbnut
+                var res = req("GET", url); 
+                console.log(res.getBody('utf8')); 
             }
         }
         writeToFile(distances);   // This Function passes the array to a file which writes it to a JSON file        
